613a0fc99830a45363dec392cc327a4e3e3d0c20
Gabriel McFadyen <gabrielmcfadyen@gmail.com>
2025-03-12 01:03:32 -0400

    Implimented Whisper transcription to .JSON

--- src/data_gathering.py
+++ src/data_gathering.py
@@ -1,16 +1,22 @@
 """data_gathring.py is used to gather and process input data for training our models."""
 
 import whisper
+import json
 
 def transcribe_audio(input_file: str, output_file: str) -> None:
-    """Transcribes an MP3 file using Whisper and saves the transcription to a text file.
+    """Transcribes an MP3 file using Whisper and saves the transcription to a JSON file.
 
     Args:
         input_file (str): Path to the MP3 file.
-        output_file (str): Path to the output text file.
+        output_file (str): Path to the output JSON file.
     """
-    model = whisper.load_model("base")
+    model = whisper.load_model("small")
     result = model.transcribe(input_file)
     
     with open(output_file, "w", encoding="utf-8") as f:
-        f.write(result["text"])
+        json.dump({"PLACEHOLDER": result["text"]}, f, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+
+    transcribe_audio(input_file="/Users/gabriel/Downloads/beginning_commentary.wav", output_file="/Users/gabriel/Documents/GitHub/EAFC-ML-Remaster/data/output.json")   
+    print("Finsihed!")
